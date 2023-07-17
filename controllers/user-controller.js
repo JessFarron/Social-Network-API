94f694c3c54a6e14e16fa23df8002a037cc30425
JessFarron <jessamesan@hotmail.com>
2023-07-16 20:52:03 -0600

    Adding working files

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,28 +1,24 @@
 const { User } = require('../models');
 
 const UserController = {
-  // 1. Get all users
   getAllUsers(req, res) {
     User.find({})
       .then(userData => res.json(userData))
       .catch(err => res.status(500).json(err));
   },
 
-  // 2. Get one user by ID
   getUserById(req, res) {
     User.findById(req.params.userId)
       .then(userData => res.json(userData))
       .catch(err => res.status(500).json(err));
   },
-  
-  // 3. Create a user
+
   createUser(req, res) {
     User.create(req.body)
       .then(userData => res.json(userData))
       .catch(err => res.status(500).json(err));
   },
 
-  // 4. Update user by ID
   updateUserById(req, res) {
     User.findOneAndUpdate(req.params.id, req.body, { new: true })
       .then(userData => {
@@ -34,7 +30,6 @@ const UserController = {
       .catch(err => res.status(500).json(err));
   },
 
-  // 5. Delete user
   deleteUserById(req, res) {
     User.findOneAndDelete(req.params.id)
       .then(userData => {
@@ -46,7 +41,6 @@ const UserController = {
       .catch(err => res.status(500).json(err));
   },
 
-  // 6. Add friend to user's friend list
   addFriend(req, res) {
     User.findOneAndUpdate(
       { _id: req.params.userId },
@@ -62,9 +56,6 @@ const UserController = {
       .catch(err => res.status(500).json(err));
   },
 
-
-  // NEW 7. Remove friend from user's friend list
-
   removeFriend({ params }, res) {
     User.findOneAndUpdate(
       { _id: params.userId },
@@ -75,9 +66,7 @@ const UserController = {
         if (!dbUserData) {
           return res.status(404).json({ message: "No user with this id!" });
         }
-        // check if friend was removed
         const removed = !dbUserData.friends.includes(params.friendId);
-        // return response with appropriate message
         if (removed) {
           res.json({ message: "Friend removed successfully!", dbUserData });
         } else {
@@ -88,6 +77,4 @@ const UserController = {
   },
 };
 
-
-// Export UserController
 module.exports = UserController;
--- models/User.js
+++ models/User.js
@@ -1,6 +1,4 @@
-// Importing the required dependencies from the mongoose library
 const { Schema, model, Types } = require('mongoose'); 
-// Defining the User schema with the required fields and their respective data types
 const userSchema = new Schema(
   {
     username: {
@@ -9,7 +7,6 @@ const userSchema = new Schema(
         unique: true,
         trim: true,
     },
-      // using regular expression to validate email format
     email: {
         type: String,
         required: true,
@@ -36,19 +33,16 @@ const userSchema = new Schema(
   },
   {
     toJSON: {
-      virtuals: true, // enables virtual properties to be displayed when a user document is transformed into JSON format
+      virtuals: true,
     },
-    id: false, // disables the default '_id' field in the User model to be returned when calling toJSON() method
+    id: false, 
 }
 );
 
-// Defining a virtual property 'friendCount' which returns the number of friends in the friends array
 userSchema.virtual('friendCount').get(function(){
     return this.friends.length;
 });
-// Creating the User model from the userSchema
 const User = model('User',userSchema)
-// Exporting the User model as a module
 module.exports = User
 
 
